56072a314867196ce9a72561ecbc1b028b2a552b
Md Ishmam Iqbal <iqbal.ishmam@gmail.com>
2023-06-13 21:51:15 +0600

    Add github pages config 2

--- src/Components/ProductsSlider.jsx
+++ src/Components/ProductsSlider.jsx
@@ -1,27 +1,29 @@
 /* eslint-disable react/prop-types */
 import capitalizeFirstLetter from "../Functions/capitalizeFirstLetter";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faChevronLeft,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
+
 import { Link } from "react-router-dom";
 
-library.add(faChevronLeft, faChevronRight);
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faChevronLeft,
+//   faChevronRight,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// library.add(faChevronLeft, faChevronRight);
 
 const ProductsSlider = ({ products }) => {
   return (
     <section className="slider-container">
       <h1 className="slider-title">Featured Products</h1>
       <div className="slider-wrapper noselect">
-        <FontAwesomeIcon
+        {/* <FontAwesomeIcon
           icon={faChevronLeft}
           onClick={() => {
             console.log("left");
           }}
           className="slider-arrow"
-        />
+        /> */}
         <div id="slider" className="slider">
           {products.map((product) => {
             let capitalizedCategory = capitalizeFirstLetter(product.category);
@@ -37,13 +39,13 @@ const ProductsSlider = ({ products }) => {
             );
           })}
         </div>
-        <FontAwesomeIcon
+        {/* <FontAwesomeIcon
           icon={faChevronRight}
           onClick={() => {
             console.log("right");
           }}
           className="slider-arrow"
-        />
+        /> */}
       </div>
     </section>
   );
