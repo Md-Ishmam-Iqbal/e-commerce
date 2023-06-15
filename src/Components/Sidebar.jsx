425a930431a81d50b0c17daadc02b271666082fd
Md Ishmam Iqbal <iqbal.ishmam@gmail.com>
2023-06-15 18:18:05 +0600

    added sidebar content

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,12 +1,44 @@
 /* eslint-disable react/prop-types */
+
+import filterProductsByCategory from "../Functions/filterProductsByCategory";
+import capitalizeFirstLetter from "../Functions/capitalizeFirstLetter";
+
+import { Link } from "react-router-dom";
+
 const Sidebar = ({ products, categories, sideBarOpen }) => {
-  return (
-    <section className={`sidebar ${sideBarOpen ? "open" : ""}`}>
-      <div>
-        <div>Sidebar</div>
-      </div>
-    </section>
-  );
+  if (!categories || !products) return null;
+  else
+    return (
+      <section className={`sidebar ${sideBarOpen ? "open" : ""}`}>
+        {categories.map((category, index) => {
+          let filteredProducts = filterProductsByCategory(products, category);
+          let capitalizedCategory = capitalizeFirstLetter(category);
+          return (
+            <div key={index} className="sidebar-category-wrapper">
+              <Link
+                to={`/${category}`}
+                className="sidebar-category-text no-txt-decoration"
+              >
+                {capitalizedCategory}
+              </Link>
+              <div className="sidebar-products-column">
+                {filteredProducts.map((product) => {
+                  return (
+                    <Link
+                      key={product.id}
+                      to={`/${category}/${product.title}`}
+                      className="sidebar-product no-txt-decoration"
+                    >
+                      {product.title}
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    );
 };
 
 export default Sidebar;
