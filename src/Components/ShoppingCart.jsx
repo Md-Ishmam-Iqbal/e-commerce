9fbc8ab9c981fdefaf55082c75377c5ad419970e
Md Ishmam Iqbal <iqbal.ishmam@gmail.com>
2023-06-23 16:56:33 +0600

    updated cart items design

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -13,57 +13,53 @@ const ShoppingCart = ({ cartOpen, products }) => {
   }
 
   return (
-    <section
-      className={`shopping-cart ${cartOpen ? "shopping-cart-open" : ""}`}
-    >
+    <div className={`shopping-cart ${cartOpen ? "shopping-cart-open" : ""}`}>
       <h1>Shopping Cart</h1>
       <h3>You have {cart.productsCount} items in your cart</h3>
-      {cart.items.map((item) => {
-        return (
-          <div key={item.id}>
-            {products.map((product) => {
-              if (product.id === item.id) {
-                return (
-                  <div key={product.id} className="cart-item-container">
-                    <Link
-                      to={`/${product.category}/${product.title}`}
-                      className="cart-item no-txt-decoration"
+      <section className="cart-items">
+        {cart.items.map((item) => {
+          return products.map((product) => {
+            if (product.id === item.id) {
+              return (
+                <div key={product.id} className="cart-item-container">
+                  <Link
+                    to={`/${product.category}/${product.title}`}
+                    className="cart-item no-txt-decoration"
+                  >
+                    <h2>{product.title}</h2>
+                  </Link>
+                  <div className="cart-quantity">
+                    <button
+                      onClick={() => cart.decreaseCartQuantity(product.id)}
+                      className="cart-quantity-operator minus"
                     >
-                      <h2>{product.title}</h2>
-                      <h3>{product.price} TK</h3>
-                    </Link>
-                    <div className="cart-quantity">
-                      <button
-                        onClick={() => cart.decreaseCartQuantity(product.id)}
-                        className="cart-quantity-operator minus"
-                      >
-                        -
-                      </button>{" "}
-                      <span className="item-quantity">{item.quantity}</span>
-                      <button
-                        onClick={() => cart.increaseCartQuantity(product.id)}
-                        className="cart-quantity-operator plus"
-                      >
-                        +
-                      </button>
-                    </div>
+                      -
+                    </button>{" "}
+                    <span className="item-quantity">{item.quantity}</span>
+                    <button
+                      onClick={() => cart.increaseCartQuantity(product.id)}
+                      className="cart-quantity-operator plus"
+                    >
+                      +
+                    </button>
                   </div>
-                );
-              }
-            })}
-          </div>
-        );
-      })}
-      {/* {products.map((product) => {
-        return (
-          <div className="shopping-cart-item" key={product.id}>
-            <h2>{product.title}</h2>
-            <h3>{product.price}</h3>
-            <h3>{product.quantity}</h3>
-          </div>
-        );
-      })} */}
-    </section>
+                  <h3 className="cart-subtotal">
+                    <span>Subtotal</span>
+                    {product.price} TK
+                  </h3>
+                </div>
+              );
+            }
+          });
+        })}
+        <button className="remove-all">Remove All</button>
+      </section>
+      <h3 className="total-bill">
+        <span>Total</span>
+        100 TK
+      </h3>
+      <button className="cart-checkout">Checkout</button>
+    </div>
   );
 };
 
