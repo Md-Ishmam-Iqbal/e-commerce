de33802f1bef27fb14dde4e42318171bfb108e35
Md Ishmam Iqbal <iqbal.ishmam@gmail.com>
2023-06-26 12:00:04 +0600

    uninstalled redux, make new redux app

---
+++ src/Pages/Checkout.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { CartContext } from "../Context/ShoppingCartContext";
+import { useContext } from "react";
+
+function Checkout({ products }) {
+  const cart = useContext(CartContext);
+  const items = cart.items;
+
+  return (
+    <div>
+      <h1>Checkout</h1>
+      {items.map((item) => {
+        return products.map((product) => {
+          if (product.id === item.id) {
+            return (
+              <section key={product.id}>
+                <h2>Product Name: {product.title}</h2>
+                <h2>Quantity: {item.quantity}</h2>
+                <button>
+                  <h3>Confirm Payment</h3>
+                </button>
+              </section>
+            );
+          }
+        });
+      })}
+    </div>
+  );
+}
+
+export default Checkout;
