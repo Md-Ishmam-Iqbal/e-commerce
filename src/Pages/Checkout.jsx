0ee94feb2be09d1b75c71f69486fb6553dfe950c
Md Ishmam Iqbal <iqbal.ishmam@gmail.com>
2023-06-27 13:12:17 +0600

    checkout page drafted structure 2

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -16,13 +16,29 @@ function Checkout({ products, sideBarOpen }) {
   const cart = useContext(CartContext);
   const items = cart.items;
 
+  function getTotalCost() {
+    let totalCost = 0;
+
+    let cartProducts = cart.items;
+
+    cartProducts.map((cartItem) => {
+      products.map((product) => {
+        if (product.id === cartItem.id) {
+          totalCost += product.price * cartItem.quantity;
+        }
+      });
+    });
+
+    return totalCost;
+  }
+
   return (
     <main
       className={`checkout-page ${
         sideBarOpen ? "checkout-page-right-margin" : ""
       }`}
     >
-      <h1>Checkout</h1>
+      <h1 className="checkout-header">Checkout</h1>
       <section className="checkout-grid">
         <ul className="headings">
           <li>Description</li>
@@ -30,23 +46,36 @@ function Checkout({ products, sideBarOpen }) {
           <li>Remove</li>
           <li>Price</li>
         </ul>
-        <ul className="checkout-product-details">
-          <li>Product Name: </li>
-          <li>Quantity: </li>
-          <li>
-            <button>Remove All</button>
-          </li>
-          <li>Price: </li>
-        </ul>
         {items.map((item) => {
           return products.map((product) => {
             if (product.id === item.id) {
               return (
                 <ul key={product.id} className="checkout-product-details">
-                  <li>{product.title}</li>
-                  <li>{item.quantity}</li>
+                  <li className="checkout-title checkout-product-text">
+                    {product.title}
+                  </li>
+                  <li className="checkout-quantity-container">
+                    <button
+                      onClick={() => cart.decreaseCartQuantity(product.id)}
+                      className="checkout-quantity-operator"
+                    >
+                      -
+                    </button>{" "}
+                    <span className="checkout-quantity">{item.quantity}</span>
+                    <button
+                      onClick={() => cart.increaseCartQuantity(product.id)}
+                      className="checkout-quantity-operator"
+                    >
+                      +
+                    </button>
+                  </li>
                   <li>
-                    <button>Remove All</button>
+                    <button
+                      onClick={() => cart.removeFromCart(product.id)}
+                      className="checkout-remove-btn"
+                    >
+                      Remove All
+                    </button>
                   </li>
                   <li>{product.price * item.quantity}</li>
                 </ul>
@@ -54,6 +83,10 @@ function Checkout({ products, sideBarOpen }) {
             }
           });
         })}
+        <div className="checkout-total">
+          <span className="total-title">Total Amount</span>
+          <span className="total-amount">{getTotalCost().toFixed(2)} TK</span>
+        </div>
         <div className="checkout-buttons">
           <button
             onClick={() => navigate("/")}
